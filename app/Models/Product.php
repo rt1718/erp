2a5e2d622cf9ecd9088d0505e9350b0613416fc8
Ilya Samsonov <sawsonov@yandex.ru>
2025-01-23 16:47:11 +0300

    add logic

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'quantity',
+        'purchase_price',
+        'sale_price',
+        'unit',
+        'image'
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
