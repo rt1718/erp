c80537256a873524f8abcc60855a1510826fe717
Ilya Samsonov <sawsonov@yandex.ru>
2025-01-23 12:34:09 +0300

    Добавлен шаблон login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,7 +8,6 @@
     <title>Авторизация</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           crossorigin="anonymous">
-{{--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>--}}
 </head>
 <body>
 <div class="container mt-5" style="max-width: 300px">
