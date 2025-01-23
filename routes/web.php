226fd24f9a9156dce062c3d24a45669d22ee7cdc
Ilya Samsonov <sawsonov@yandex.ru>
2025-01-23 12:16:41 +0300

    s

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\AuthorizationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::fallback(function () {
+    abort(404);
+});
+
+Route::prefix('login')
+    ->middleware('guest')
+    ->group(function () {
+        Route::get('/', [AuthorizationController::class, 'index'])
+            ->name('login');
+        Route::post('/', [AuthorizationController::class, 'login']);
+    });
+
+Route::prefix('dashboard')
+    ->middleware('login')
+    ->group(function () {
+    Route::get('/', [AdminController::class, 'index'])
+        ->name('admin');
 });
