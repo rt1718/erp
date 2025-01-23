57bdefdcad0b85229f6eee381bbc8837b95f67b7
Ilya Samsonov <sawsonov@yandex.ru>
2025-01-23 12:31:56 +0300

    Добавлен шаблон login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,15 +1,33 @@
 <!doctype html>
-<html lang="{{ env('APP_LOCALE') }}">
+<html lang="{{ env('APP_LOCALE') }}" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
-
+    <title>Авторизация</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          crossorigin="anonymous">
+{{--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>--}}
 </head>
 <body>
+<div class="container mt-5" style="max-width: 300px">
+    <form action="{{ route('login.post') }}" method="post">
 
+        @csrf
+
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input name="email" type="email" class="form-control" id="email">
+        </div>
+
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input name="password" type="password" class="form-control" id="password">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
 </body>
 </html>
