7f52d1d3833db8b2c807e37cc7233cbd1c7d399e
Ilya Samsonov <sawsonov@yandex.ru>
2025-01-25 22:53:57 +0300

    логика

--- app/Http/Controllers/WriteOffController.php
+++ app/Http/Controllers/WriteOffController.php
@@ -2,22 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreWriteOffRequest;
 use App\Models\Product;
-use Illuminate\Http\Request;
+use App\Models\WriteOff;
 
 class WriteOffController extends Controller
 {
     public function create()
     {
-        $products = Product::query()->get();
+        $products = Product::query()->where([
+            ['quantity', '!=', 0],
+            ['purchase_price', '!=', 0],
+        ])
+            ->get();
 
-        return view('writeOff.create', [
+        return view('admin.writeOff.create', [
             'products' => $products,
         ]);
     }
 
-    public function store()
+    public function store(StoreWriteOffRequest $request)
     {
-        
+        $validatedData = $request->validated();
+
+        foreach ($validatedData['products'] as $productId => $data) {
+            $product = Product::query()->findOrFail($productId);
+
+            if ($product) {
+                // Вычитаем количество из products
+                $newQuantity = $product->quantity - ($data['quantity'] ?? 0);
+
+                if ($newQuantity < 0) {
+                    return redirect()->back()->withErrors([
+                        'quantity' => "Недостаточно товара на складе для списания.",
+                    ])->withInput();
+                }
+
+                $product->update([
+                    'quantity' => $newQuantity,
+                ]);
+
+                // Создаём запись в таблице WriteOff
+                WriteOff::query()->create([
+                    'title' => $data['product_title'],
+                    'product_id' => $productId,
+                    'quantity' => $data['quantity'] ?? 0, // Количество, которое списали
+                ]);
+            }
+        }
+
+        return redirect()->route('write-off.create')->with('success', 'Добавлено!');
     }
 }
--- app/Models/WriteOff.php
+++ app/Models/WriteOff.php
@@ -3,14 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class WriteOff extends Model
 {
-    protected $table = 'supplies';
+    protected $table = 'write_off';
 
     protected $fillable = [
         'product_id',
         'title',
         'quantity',
     ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
