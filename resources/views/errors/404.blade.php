51c2efcbca37b24ba55cbfdb4397f69aea46ee3a
Ilya Samsonov <sawsonov@yandex.ru>
2025-01-27 00:13:01 +0300

    апач стандартный 404 + бутсрап

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,12 +1,34 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang=ru>
 <head>
-    <title>404 Not Found</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Not found</title>
 </head>
 <body>
-<h1>Not Found</h1>
-<p>The requested URL was not found on this server.</p>
-<hr>
-<address>Apache Server at localhost Port 80</address>
+<div class="container text-center position-absolute top-50 start-50 translate-middle">
+    <h1>
+        404
+    </h1>
+    <p>Not found</p>
+</div>
 </body>
 </html>
+
+
+{{--<!DOCTYPE html>--}}
+{{--<html>--}}
+{{--<head>--}}
+{{--    <title>404 Not Found</title>--}}
+{{--</head>--}}
+{{--<body>--}}
+{{--<h1>Not Found</h1>--}}
+{{--<p>The requested URL was not found on this server.</p>--}}
+{{--<hr>--}}
+{{--<address>Apache Server at localhost Port 80</address>--}}
+{{--</body>--}}
+{{--</html>--}}
